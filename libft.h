c4422c52c85ea38f6c4cef0d33fed4ae4530939a
nbidal <nizarbidal18@gmail.com>
2023-12-25 12:10:47 +0100

    sync

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 19:16:38 by nbidal            #+#    #+#             */
-/*   Updated: 2023/12/24 19:16:39 by nbidal           ###   ########.fr       */
+/*   Updated: 2023/12/24 20:51:49 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ int	ft_isprint(unsigned char c);
 int	ft_strlen(const char *s);
 void *memset(void *s, int c, size_t n);
 void bzero(void *s, size_t n);
-void *memcpy(void *restrict dst, const void *restrict src, size_t n);
-size_t strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
+//because restrict is just an optimization detail we omit it here
+//also it's a .c qualifier and not a .h
+void *memcpy(void *dst, const void *src, size_t n);
+size_t strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
