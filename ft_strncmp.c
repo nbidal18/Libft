4aac799aefaa7a6c85eb74e289a1b1b05d9f9e45
nbidal18 <nizarbidal18@gmail.com>
2024-01-30 19:35:48 +0100

    back one version, one error left

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,12 +6,11 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 18:00:16 by nbidal            #+#    #+#             */
-/*   Updated: 2024/01/30 19:13:48 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/01/30 19:35:17 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -20,29 +19,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		if ((s1[i] >= 0 && s1[i] <= 127) && (s2[i] >= 0 && s2[i] <= 127))
-		{
-			if ((s1[i] > s2[i]) || (s1[i] != '\0' && s2[i] == '\0'))
-				return (1);
-			else if ((s1[i] < s2[i]) || (s1[i] == '\0' && s2[i] != '\0'))
-				return (-1);
-		}
-		else
-		{
-			if (s1[i] > s2[i])
-				return (1);
-			else if (s1[i] < s2[i])
-			{
-				printf("here %zu\n", i);
-				return (-1);
-			}
-		}
+		if ((s1[i] > s2[i]) || (s1[i] != '\0' && s2[i] == '\0'))
+			return (1);
+		else if ((s1[i] < s2[i]) || (s1[i] == '\0' && s2[i] != '\0'))
+			return (-1);
 		i++;
 	}
 	return (0);
 }
 
-int main() {
+/*int main() {
 	int result = ft_strncmp("\x12\xff\x65\x12\xbd\xde\xad", "\x12\x02", 6);
 
     if (result == 0) {
@@ -54,4 +40,4 @@ int main() {
     }
 
     return 0;
-}
+}*/
