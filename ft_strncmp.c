542e75cbafcffa518f2d24c7b6405bd8d65ceed8
nbidal18 <nizarbidal18@gmail.com>
2024-01-30 19:00:19 +0100

    one error left

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 18:00:16 by nbidal            #+#    #+#             */
-/*   Updated: 2024/01/30 18:54:14 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/01/30 18:59:33 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
 		if ((s1[i] > s2[i]) || (s1[i] != '\0' && s2[i] == '\0'))
 			return (1);
-		if ((s1[i] < s2[i]) || (s1[i] == '\0' && s2[i] != '\0'))
+		else if ((s1[i] < s2[i]) || (s1[i] == '\0' && s2[i] != '\0'))
 			return (-1);
 		i++;
 	}
