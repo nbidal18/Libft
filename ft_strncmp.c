b265bf0cd1fdde9578baf900922848b56b49a7fd
nbidal18 <nizarbidal18@gmail.com>
2024-01-30 18:54:21 +0100

    ft_strncmp still has some errors

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,16 +6,39 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 18:00:16 by nbidal            #+#    #+#             */
-/*   Updated: 2024/01/30 18:03:50 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/01/30 18:54:14 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
 
-int	strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	size_t	i;
+
+	i = 0;
+	while (i < n)
 	{
-		
+		if ((s1[i] > s2[i]) || (s1[i] != '\0' && s2[i] == '\0'))
+			return (1);
+		if ((s1[i] < s2[i]) || (s1[i] == '\0' && s2[i] != '\0'))
+			return (-1);
+		i++;
 	}
+	return (0);
 }
+
+/*int main() {
+	int result = ft_strncmp("test\200", "test\0", 6);
+
+    if (result == 0) {
+        printf("The strings are equal.\n");
+    } else if (result > 0) {
+        printf("str1 is greater than str2.\n");
+    } else {
+        printf("str1 is less than str2.\n");
+    }
+
+    return 0;
+}*/
