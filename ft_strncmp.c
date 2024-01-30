5b956b5318b9d221549cb26993ad054ef211d1cd
nbidal18 <nizarbidal18@gmail.com>
2024-01-30 20:39:46 +0100

    fixed ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,36 +6,26 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 18:00:16 by nbidal            #+#    #+#             */
-/*   Updated: 2024/01/30 20:03:11 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/01/30 20:38:59 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*int	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}*/
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	if (n == 0)
+		return (0);
+	while (i < n - 1 && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		if ((s1[i] > s2[i]) || (s1[i] != '\0' && s2[i] == '\0'))
-			return (1);
-		else if ((s1[i] < s2[i]) || (s1[i] == '\0' && s2[i] != '\0'))
-			return (-1);
+		if (s1[i] != s2[i])
+			break ;
 		i++;
 	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 /*int main() {
