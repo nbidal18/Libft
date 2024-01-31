c937f93b5213b1fcdade06fcbe1dba3de3be5d82
nbidal18 <nizarbidal18@gmail.com>
2024-01-31 15:44:27 +0100

    correct (tested by francinette)

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 15:03:20 by nbidal            #+#    #+#             */
-/*   Updated: 2024/01/31 15:05:01 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/01/31 15:36:53 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	size_t			i;
+	unsigned char	*s11;
+	unsigned char	*s22;
 
+	i = 0;
+	s11 = (unsigned char *)s1;
+	s22 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (i < n - 1)
+	{
+		if (s11[i] != s22[i])
+			break ;
+		i++;
+	}
+	return (s11[i] - s22[i]);
 }
