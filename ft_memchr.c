cbef79eba7210d03b0be9d3d3a6ed5115441f770
nbidal18 <nizarbidal18@gmail.com>
2024-01-31 14:17:29 +0100

    made some progress but still have a couple of errors

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 10:01:54 by nbidal            #+#    #+#             */
-/*   Updated: 2024/01/31 10:03:12 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/01/31 14:15:54 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	
+	size_t			i;
+	unsigned char	*s2;
+
+	i = 0;
+	s2 = (unsigned char *)s;
+	c = (unsigned char)c;
+	if (n == 0)
+		return (NULL);
+	if (s2[i] == '\0')
+		return (s2 + i);
+	while (i < n - 1 && s2[i] != '\0')
+	{
+		if (s2[i] == c)
+			return (s2 + i);
+		i++;
+	}
+	return (NULL);
 }
