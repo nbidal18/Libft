2298200b7001575d95456dec0960b9125af74f6b
nbidal18 <nizarbidal18@gmail.com>
2024-01-31 15:05:43 +0100

    added ft_memcmp, need to work on it

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 19:16:38 by nbidal            #+#    #+#             */
-/*   Updated: 2024/01/31 10:02:52 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/01/31 15:05:13 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,5 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
