cfcd3db0a3dbc8d98f1c45a75fed7faf361b4aab
nbidal18 <nizarbidal18@gmail.com>
2024-01-31 16:23:32 +0100

    added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 19:16:38 by nbidal            #+#    #+#             */
-/*   Updated: 2024/01/31 15:05:13 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/01/31 16:22:37 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,5 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 #endif
