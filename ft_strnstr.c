d20efbdf392c6735f61a1dcf65ef82c507b397be
nbidal18 <nizarbidal18@gmail.com>
2024-02-01 14:03:18 +0100

    quick push

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,14 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 16:17:14 by nbidal            #+#    #+#             */
-/*   Updated: 2024/01/31 16:58:03 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/02/01 13:54:41 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-/*int	ft_strlen(const char *s)
+int	ft_strlen(const char *s)
 {
 	int	i;
 
@@ -20,7 +21,7 @@
 	while (s[i] != '\0')
 		i++;
 	return (i);
-}*/
+}
 
 //lorem
 //lorem ipsum dolor sit amet
@@ -48,7 +49,20 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 			i = 0;
 		}
 	}
+	printf("i: %zu\n", i);
 	if ((size_t)ft_strlen(needle) == i)
 		return (start);
 	return (NULL);
+}
+
+int main()
+{
+	char * big = "see FF your FF return FF now FF";
+	char * little = "FF";
+	size_t max = ft_strlen(big);
+	char *result = ft_strnstr(big, little, max);
+	if(result != NULL)
+		printf("found, %c\n", *result);
+	else if (result == NULL)
+		printf("not found\n");
 }
