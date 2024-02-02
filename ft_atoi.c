58d4efa1e18bc8ec7420f2c5439476c0ea766078
nbidal18 <nizarbidal18@gmail.com>
2024-02-02 10:01:44 +0100

    added

---
+++ ft_atoi.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 09:50:28 by nbidal            #+#    #+#             */
+/*   Updated: 2024/02/02 09:54:47 by nbidal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//input: 123abc456
+//output: 123
+
+//input: 123 456abc678
+//output: 123
+
+//reads the input
+	//if a digit, convert && go to next
+	//else stop
+
+//spaces are also non digit
+//stops at non digit or at end of string, which is also not a digit
+
+int	ft_atoi(const char *str)
+{
+	
+}
