015ab341acbbea32f4799aa989ab9fbf1b3cfb9f
nbidal18 <nizarbidal18@gmail.com>
2024-02-05 12:39:29 +0100

    added, has some errors to fix

---
+++ ft_substr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 12:02:48 by nbidal            #+#    #+#             */
+/*   Updated: 2024/02/05 12:26:22 by nbidal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)malloc(len + 1 * sizeof(char));
+	if (str == NULL || start >= (unsigned)ft_strlen(s))
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
