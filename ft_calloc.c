708fd14c4f97153d7181b8c482b807469d18d03d
nbidal18 <nizarbidal18@gmail.com>
2024-02-05 11:22:42 +0100

    should be correct as the other version ,but gives an error on mac, need to check on linux

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 10:51:25 by nbidal            #+#    #+#             */
-/*   Updated: 2024/02/05 11:07:32 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/02/05 11:13:32 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	total_size;
 	void	*ptr;
 
-	total_size = count * size;
-	ptr = malloc(total_size);
+	ptr = (void *)malloc(count * sizeof(size));
 	if (ptr != NULL)
-		ft_memset(ptr, 0, total_size);
+		ft_memset(ptr, 0, count * size);
 	return (ptr);
 }
