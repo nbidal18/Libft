1eefbbf5a7a6444fea960b1d25b20819500140ee
nbidal18 <nizarbidal18@gmail.com>
2024-02-05 11:07:36 +0100

    corect ()

---
+++ ft_calloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 10:51:25 by nbidal            #+#    #+#             */
+/*   Updated: 2024/02/05 11:07:32 by nbidal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	total_size;
+	void	*ptr;
+
+	total_size = count * size;
+	ptr = malloc(total_size);
+	if (ptr != NULL)
+		ft_memset(ptr, 0, total_size);
+	return (ptr);
+}
