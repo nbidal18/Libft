05e43ad9b1fbaaf8097fb05f41a2df400916f0e9
nbidal18 <nizarbidal18@gmail.com>
2024-02-05 10:49:00 +0100

    correct (tested by francinette)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,24 +6,46 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 09:50:28 by nbidal            #+#    #+#             */
-/*   Updated: 2024/02/02 09:54:47 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/02/05 10:48:15 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//input: 123abc456
-//output: 123
-
-//input: 123 456abc678
-//output: 123
-
-//reads the input
-	//if a digit, convert && go to next
-	//else stop
-
-//spaces are also non digit
-//stops at non digit or at end of string, which is also not a digit
+#include <stdlib.h>
+//#include <stdio.h>
 
 int	ft_atoi(const char *str)
 {
-	
+	int	i;
+	int	result;
+	int	sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	if (sign == -1)
+		return (result * sign);
+	return (result);
 }
+
+/*int main(void)
+{
+    const char *testString = "-4886";
+
+    int result = ft_atoi(testString);
+    printf("Input: \"%s\"  |  Output: %d\n", testString, result);
+
+    return 0;
+}*/
