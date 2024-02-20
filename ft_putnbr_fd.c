bd479d669f7f8a68f49510ade3d5b3e25eb4ae22
nbidal18 <nizarbidal18@gmail.com>
2024-02-20 10:54:45 +0100

    added in comments how it might have to be done

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 10:19:33 by nbidal            #+#    #+#             */
-/*   Updated: 2024/02/20 10:43:06 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/02/20 10:54:34 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,28 @@ void	ft_putnbr_fd(int n, int fd)
 		}
 	}
 }
+
+/*
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+	{
+		if (-2147483648 == n)
+		{
+			ft_putstr_fd("-2147483648", fd);
+			return ;
+		}
+		n *= -1;
+		write(fd, "-", 1);
+	}	
+	if (0 == n)
+		write(fd, "0", 1);
+	else if (n > 0 && n < 10)
+		ft_putchar_fd((n + 48), fd);
+	else
+	{
+		ft_putnbr_fd((n / 10), fd);
+		ft_putchar_fd((n % 10) + 48, fd);
+	}
+}
+*/
