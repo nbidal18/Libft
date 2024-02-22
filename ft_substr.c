dfa13a5ffbcbf16ef9505734e679e14f69dbeeb8
nbidal <nizarbidal18@gmail.com>
2024-02-22 18:57:53 +0100

    correct (tested by francinette)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 12:02:48 by nbidal            #+#    #+#             */
-/*   Updated: 2024/02/12 15:12:34 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/02/22 18:52:04 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,27 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	i;
-	size_t	lens;
 
-	i = -1;
-	lens = ft_strlen(s);
-	if (start > (unsigned int)ft_strlen(s))
+	i = 0;
+	if (start >= (unsigned int)ft_strlen(s))
 	{
-		substr = malloc(1 * sizeof(char));
+		substr = ft_calloc(1, sizeof(char));
 		if (substr == NULL)
 			return (NULL);
-		substr[0] = '\0';
 		return (substr);
 	}
 	s = s + start;
-	if (lens <= len)
+	if ((size_t)ft_strlen(s) <= len)
 		substr = malloc((ft_strlen(s) + 1) * sizeof(char));
 	else
 		substr = malloc((len + 1) * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
-	while (s[++i] != '\0' && i < len)
+	while (s[i] != '\0' && i < len)
+	{
 		substr[i] = s[i];
+		i++;
+	}
 	substr[i] = '\0';
 	return (substr);
 }
