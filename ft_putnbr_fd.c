846720e39cbd2cd15af5fb1aa64fc17df2425021
nbidal <nizarbidal18@gmail.com>
2024-02-22 15:16:44 +0100

    correct (tested by francinette)

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 10:19:33 by nbidal            #+#    #+#             */
-/*   Updated: 2024/02/20 10:54:34 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/02/22 15:14:48 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,34 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*converted_n;
-	int		len;
+	int		i;
+	char	str[12];
+	long	nbr;
 
-	if (!(fd < 0))
+	nbr = n;
+	if (nbr == 0)
+		write (fd, "0", 1);
+	i = 0;
+	if (nbr < 0)
 	{
-		converted_n = ft_itoa(n);
-		len = ft_strlen(converted_n);
-		while (len-- > 0 && *converted_n != '\0')
-		{
-			write(fd, converted_n, 1);
-			converted_n++;
-		}
+		write (fd, "-", 1);
+		nbr = nbr * -1;
 	}
+	while (nbr > 0)
+	{
+		str[i++] = (nbr % 10) + 48;
+		nbr = nbr / 10;
+	}
+	str[i + 1] = 0;
+	i--;
+	while (i >= 0)
+		write (fd, &str[i--], 1);
 }
 
-/*
-void	ft_putnbr_fd(int n, int fd)
+/*int main(int argc, char **argv)
 {
-	if (n < 0)
-	{
-		if (-2147483648 == n)
-		{
-			ft_putstr_fd("-2147483648", fd);
-			return ;
-		}
-		n *= -1;
-		write(fd, "-", 1);
-	}	
-	if (0 == n)
-		write(fd, "0", 1);
-	else if (n > 0 && n < 10)
-		ft_putchar_fd((n + 48), fd);
-	else
-	{
-		ft_putnbr_fd((n / 10), fd);
-		ft_putchar_fd((n % 10) + 48, fd);
-	}
-}
-*/
+	if (argc < 1)
+		return (0);
+	int nbr = ft_atoi(argv[1]);
+	ft_putnbr_fd(nbr, 1);
+}*/
