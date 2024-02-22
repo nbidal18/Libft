e47f925abc07d83202168107633dbc97538ebaab
nbidal <nizarbidal18@gmail.com>
2024-02-22 15:47:34 +0100

    correct (tested by francinette)

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 10:05:21 by nbidal            #+#    #+#             */
-/*   Updated: 2024/02/20 10:38:17 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/02/22 15:36:01 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putendl_fd(char *s, int fd)
 	int	len;
 	int	newline;
 
-	if (!(fd < 0))
+	if (!(fd <= 0))
 	{
 		len = ft_strlen(s);
 		newline = '\n';
