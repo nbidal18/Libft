abbf8c08e7fb28116a162e550b1fb34f34d9f061
nbidal <nizarbidal18@gmail.com>
2024-03-15 14:27:01 +0100

    Started the bonus

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 11:42:12 by nbidal            #+#    #+#             */
-/*   Updated: 2024/03/15 11:50:05 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/03/15 14:23:55 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*tmp;
 
-	new->next = NULL;
 	if (*lst == NULL)
 		*lst = new;
 	else
